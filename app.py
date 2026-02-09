fd05ef49f3c54f4e543db13926412ae1dbb278f4
SerafinPinaBarranco <arespin23@gmail.com>
2026-02-09 13:43:02 +0100

    conexion configurada

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 import streamlit as st
+from model import OracleConnection
 
-st.set_page_config(page_title="Stadísticas Streamlit", page_icon="📊")
 
-st.markdown("# Bienvenido a Stadísticas Streamlit! 📊")
+def main():
+    st.set_page_config(page_title="Stadísticas Streamlit", page_icon="📊")
+
+    st.markdown("# Bienvenido a Stadísticas Streamlit! 📊")
+    st.write("Conectando a la base de datos Oracle...")
+    oracle_conn = OracleConnection()
+    conn = oracle_conn.get_connection()
+
+    if conn:
+        print("Conexión exitosa a la base de datos Oracle!")
+        st.success("¡Conexión exitosa a la base de datos Oracle!")
+        # Aquí puedes agregar más lógica para interactuar con la base de datos
+        conn.close()
+    else:
+        st.error("No se pudo conectar a la base de datos Oracle.")
+
+if __name__ == "__main__":
+    main()
---
+++ model.py
@@ -0,0 +1,22 @@
+import oracledb
+import streamlit as st
+
+class OracleConnection:
+    def __init__(self):
+        # Acceder a los secretos definidos en el archivo TOML
+        self.creds = st.secrets["oracle"]
+
+    def get_connection(self):
+        try:
+            # Conexión en modo Thin usando los datos del TOML
+            conn = oracledb.connect(
+                user=self.creds["user"],
+                password=self.creds["password"],
+                host=self.creds["host"],
+                port=self.creds["port"],
+                service_name=self.creds["service_name"]
+            )
+            return conn
+        except oracledb.Error as e:
+            st.error(f"Error de conexión: {e}")
+            return None
